d8c45487a7ee0f4660b3e8f86fabee486d572983
Hector <thegreatcarradio@yahoo.com>
2021-09-27 18:37:05 +0000

    Set up nginx to use env variables
    Had to use a shell script to process all the templates for each site. So that included refactoring the docker/nginx to have a ./templates and ./sites.
    The big problem I ran into was when a .conf file uses many variables. Apart of that shell script was to help preserve those variables by adding DOLLAR var that just holds '$'. then I needed to find each nginx var in the file and add  where the normal $ would be. Will need to set up a conditional in the static.conf to check if requests for svgs are coming from folio or blog and so set the CORS resonse header appropriately.

---
+++ docker/nginx/nginx_env.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+export DOLLAR='$'
+src_dir='/etc/nginx/conf.d/templates'
+target_dir='/etc/nginx/conf.d/'
+templates=$(ls $src_dir)
+
+for i in $templates
+do
+	name="${i%.*}"
+	template=$src_dir"/"$i
+
+	envsubst < $template > /etc/nginx/conf.d/$name
+	
+done
+
+nginx -g "daemon off;"
+
