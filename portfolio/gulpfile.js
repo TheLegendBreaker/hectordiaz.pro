92f95e91e22f6ee746a743f2faa3b4b9b92b5d63
Hector <thegreatcarradio@yahoo.com>
2021-12-13 18:51:27 +0000

    Added set up for Gulp and Bable

---
+++ portfolio/gulpfile.js
@@ -0,0 +1,33 @@
+// Import `src` and `dest` from gulp for use in the task.
+const { src, dest } = require("gulp")
+
+// Import Gulp plugins.
+const babel = require("gulp-babel")
+const plumber = require("gulp-plumber")
+
+// Gulp 4 uses exported objects as its tasks. Here we only have a
+// single export that represents the default gulp task.
+exports.default = function (done) {
+  // This will grab any file within src/components or its
+  // subdirectories, then ...
+  return (
+    src("./change to local src folder/*.js")
+      // Stop the process if an error is thrown.
+      .pipe(plumber())
+      // Transpile the JS code using Babel's preset-env.
+      .pipe(
+        babel({
+          presets: [
+            [
+              "@babel/env",
+              {
+                modules: false
+              }
+            ]
+          ]
+        })
+      )
+      // Save each component as a separate file in dist.
+      .pipe(dest("./dist"))
+  )
+}
